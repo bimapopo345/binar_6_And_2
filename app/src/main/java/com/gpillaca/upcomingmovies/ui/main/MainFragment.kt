90fc6e40c83a2fc8c794537cb4ccd77621787479
bimapopo345 <bimapopo345@gmail.com>
2024-06-09 14:25:14 +0700

    good

---
+++ app/src/main/java/com/gpillaca/upcomingmovies/ui/main/MainFragment.kt
@@ -0,0 +1,84 @@
+package com.gpillaca.upcomingmovies.ui.main
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.gpillaca.upcomingmovies.R
+import com.gpillaca.upcomingmovies.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private lateinit var binding: FragmentMainBinding
+
+    private val mainViewModel: MainViewModel by viewModels()
+
+    private val adapter = MoviesAdapter{ movie ->
+        mainState.onMovieClick(movie.id)
+    }
+
+    private lateinit var mainState: MainState
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+
+        mainState = buildMainState()
+
+        binding.recyclerMovies.adapter = adapter
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.state.map { it.loading }.distinctUntilChanged().collect{ isLoading ->
+                    if (binding.swipeRefresh.isRefreshing && isLoading.not()) {
+                        binding.swipeRefresh.isRefreshing = false
+                    }
+
+                    if (binding.swipeRefresh.isRefreshing.not()) {
+                        binding.progress.isVisible = isLoading
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                with(mainViewModel.state) {
+                    map { state -> state.movies }.distinctUntilChanged().collect { movies ->
+                        movies?.let {
+                            adapter.submitList(it)
+                        }
+                    }
+                 }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.state.collect{ error ->
+                    error.error?.let {
+                        Toast.makeText(requireActivity(), mainState.errorToString(it), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        mainState.requestLocationPermissions {
+            mainViewModel.onUiReady()
+        }
+
+        binding.swipeRefresh.setOnRefreshListener {
+            mainViewModel.requestPopularMovies()
+        }
+    }
+}
